1ac5b6457db298dd0af87501f7f41fb8e1fc6278
p-girija <2013girijapriya@gmail.com>
2016-08-24 14:14:36 +0530

    Add files via upload

---
+++ pos.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class gayu {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int S;
+		S=in.nextInt();
+		if(S>0)
+			System.out.println("given no is positive");
+		if(S<0)
+			System.out.println("given no is negative");
+		
+		if(S==0)
+			System.out.println("given no is zero");
+			
+		// TODO Auto-generated method stub
+
+	}
+
+}
